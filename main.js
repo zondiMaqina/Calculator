e08b0610d266e6ac0cff2cd11f42d7b2e721814c
zondimaqina <zondimaqina0@gmail.com>
2024-03-21 21:43:16 +0200

    remove literal functions

--- main.js
+++ main.js
@@ -12,92 +12,88 @@ let operand_one;
 let operand_two;
 let operator;
 
+let nine =
+document.getElementById("nine") 
+
  // digit function
 function num0(){
     input.value += 0;
 }
 
 function num1(){
-    input.value += 1;
+    input.value += 1
 }
 
 function num2(){
-    
+    input.value += 2;
+
 }
 
 function num3(){
-    
+    input.value += 3;
+ 
 }
 
 function num4(){
-    
+    input.value += 4;
+
 }
 
 function num5(){
-    
+    input.value += 5;
+
 }
 
 function num6(){
-    
-}
+    input.value += 6;
 
-function num6(){
-    
 }
 
 function num7(){
-    
+    input.value += 7;
+
 }
 
 function num8(){
-    
+    input.value += 8;
+
 }
 
 function num9(){
-    
+    input.value += nine.textContent;
+
 }
 
 function period(){
+    input.value += ".";
 
 }
 
 
 function operate(){
 
-    if (operator == add){
-        return operand_one + operand_two ;
-
-    } else if(operator = subtract){
-        return operand_one - operand_two ;
-
-    } else if (operator == multiply){
-        return operand_one * operand_two ;
-
-    }else if(operator == divide){
-        return operand_one / operand_two ;
-    }
-    
 }
 
 
 // arithmetic operators
 
 function subtract(){
-    console.log('-');
 }
 
 function add(){
-    console.log("+")
 }
 
 function multiply(){
-    console.log("*")
 }
 
 function divide(){
-    console.log("/")
 }
 
 function remove(){
-    console.log("clear")
+    input.value = null;
+}
+
+
+function equal(){
+
 }
