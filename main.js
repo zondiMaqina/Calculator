cc578a75f263b3daeae26c3bbacb9ef3f6a75cfe
zondimaqina <zondimaqina0@gmail.com>
2024-03-21 23:08:52 +0200

    add eventlisteners

--- main.js
+++ main.js
@@ -1,98 +1,60 @@
-// user chooses first button for first number
-// user chooses operator for sum
-// user chooses second number
-// presses equal to see sum
 
+// storage buttons
+let addition =
+document.getElementById("addition") ;
 
-// storage
+let subtraction =
+document.getElementById("subtract") ;
+
+let multiplication =
+document.getElementById("multiply") ;
+
+let division =
+document.getElementById("divide") ;
+
 let input =
 document.getElementById("display") ;
 
-let operand_one;
-let operand_two;
-let operator;
-
-let nine =
-document.getElementById("nine") 
-
- // digit function
-function num0(){
-    input.value += 0;
-}
-
-function num1(){
-    input.value += 1
-}
-
-function num2(){
-    input.value += 2;
-
-}
-
-function num3(){
-    input.value += 3;
- 
-}
-
-function num4(){
-    input.value += 4;
-
-}
-
-function num5(){
-    input.value += 5;
-
-}
-
-function num6(){
-    input.value += 6;
-
-}
-
-function num7(){
-    input.value += 7;
-
-}
-
-function num8(){
-    input.value += 8;
-
-}
-
-function num9(){
-    input.value += nine.textContent;
-
-}
-
-function period(){
-    input.value += ".";
-
-}
+let number_option =
+document.querySelectorAll(".num-opt");
 
 
-function operate(){
+ // Each digit to display on their own on display screen;
+let eachNumber = number_option.forEach((numberButton) => {
 
-}
-
+    numberButton.addEventListener("click", ()=>{
+        return input.value += numberButton.innerText;
+    })
+});
 
 // arithmetic operators
 
-function subtract(){
-}
+addition.addEventListener("click", function() {
+    return "hello"
+})
 
-function add(){
-}
+subtraction.addEventListener("click", function() {
+    return "hello"
+})
 
-function multiply(){
-}
+division.addEventListener("click", function() {
+    return "hello"
+})
 
-function divide(){
-}
+multiplication.addEventListener("click",function() {
+
+})
+
 
 function remove(){
     input.value = null;
 }
 
+
+// operation function
+function operate(){
+
+}
 
 function equal(){
 
