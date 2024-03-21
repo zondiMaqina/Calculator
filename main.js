031372e139a4f4e19955214d5e0f1b87ea185cb4
zondimaqina <zondimaqina0@gmail.com>
2024-03-21 15:03:31 +0200

    include functions for operators and numbers

---
+++ main.js
@@ -0,0 +1,91 @@
+// user chooses first button for first number
+// user chooses operator for sum
+// user chooses second number
+// presses equal to see sum
+
+
+// storage
+let input =
+document.getElementById("display") ;
+
+let operand_one;
+let operand_two;
+let operator;
+
+/* // digit function
+function num0(){
+    
+}
+
+function num1(){
+
+}
+
+function num2(){
+    
+}
+
+function num3(){
+    
+}
+
+function num4(){
+    
+}
+
+function num5(){
+    
+}
+
+function num6(){
+    
+}
+
+function num6(){
+    
+}
+
+function num7(){
+    
+}
+
+function num8(){
+    
+}
+
+function num9(){
+    
+}
+
+function period(){
+
+}
+ */
+
+function operate(){
+
+    
+}
+
+
+// arithmetic operators
+
+function subtract(){
+    console.log('-');
+}
+
+function add(){
+    console.log("+")
+}
+
+function multiply(){
+    console.log("*")
+}
+
+function divide(){
+    console.log("/")
+}
+
+function remove(){
+    console.log("8")
+}
