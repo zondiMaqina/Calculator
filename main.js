d875ec6df367d8f9e692a586551fb94452b58b38
Zondi Maqina <zondimaqina0@gmail.com>
2026-04-09 14:35:36 -0400

    add cunftions for checking condition of the global variables

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 const calculatorOuput = document.querySelector('input');
 const allOperators = {add: '+', subtract: '-', multiply: 'x', division: '/'};
 let num1, num2, operatorChosen;
+
+// when you want to run any operator or equal sign
+function isCalculatorOutputEmpty() {
+    return calculatorOuput.value === '';
+}
+
+// when yopu want to run equal sign
+function areNumbersSavedAvailable() {
+    return num1 !== undefined && num2 !== undefined ;
+}
