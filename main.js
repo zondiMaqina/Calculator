4f5451a3df278d75828dc86cbf88b18aeac17f55
Zondi Maqina <zondimaqina0@gmail.com>
2026-04-09 15:08:20 -0400

    add functions for saving numbers on output

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const calculatorOuput = document.querySelector('input');
-const allOperators = {add: '+', subtract: '-', multiply: 'x', division: '/'};
 let num1, num2, operatorChosen;
 
 // when you want to run any operator or equal sign
@@ -10,4 +9,45 @@ function isCalculatorOutputEmpty() {
 // when yopu want to run equal sign
 function areNumbersSavedAvailable() {
     return num1 !== undefined && num2 !== undefined ;
+}
+
+// operator is a node
+function returnOperatorPressed(operator) {
+    if (['+', '-', '/', 'x'].includes(operator.textContent)) {
+        operatorChosen = operator.textContent;
+        return operatorChosen;
+    };
+}
+
+function saveNum1OnOutput() {
+    num1 = Number(calculatorOuput.textContent);
+}
+
+function saveNum2OnOutput() {
+    num2 = Number(calculatorOuput.textContent);
+}
+
+// saves current operation on operands to num1
+function saveOperationOnNumbers(operatorPressed, number1, number2) {
+    switch (operatorPressed) {
+        case '+':
+            num1 = number1 + number2;
+            break;
+        case '-':
+            num1 = number1 - number2;
+            break;
+        case 'x':
+            num1 = number1 * number2;
+            break;
+        case '/':
+            num1 = number1 / number2;
+            break;
+    }
+    num2 = undefined;
+}
+
+function resetAll() {
+    num1 = undefined;
+    num2 = undefined;
+    operatorChosen = undefined;
 }
