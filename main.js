df7b2e765a5714f69a22e39ef20eb1db485550f9
Zondi Maqina <zondimaqina0@gmail.com>
2026-04-10 08:12:57 -0400

    add function for saving first operand and operator clicked

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const calculatorOuput = document.querySelector('input');
 let num1, num2, operatorChosen;
 
 // when you want to run any operator or equal sign
-function isCalculatorOutputEmpty() {
-    return calculatorOuput.value === '';
+function isCalculatorOutputNotEmpty() {
+    return calculatorOuput.value !== '';
 }
 
 // when yopu want to run equal sign
@@ -12,19 +12,17 @@ function areNumbersSavedAvailable() {
 }
 
 // operator is a node
-function returnOperatorPressed(operator) {
-    if (['+', '-', '/', 'x'].includes(operator.textContent)) {
+function saveOperatorPressed(operator) {
+    if (['+', '-', '/', 'x'].includes(operator.textContent)) 
         operatorChosen = operator.textContent;
-        return operatorChosen;
-    };
 }
 
 function saveNum1OnOutput() {
-    num1 = Number(calculatorOuput.textContent);
+    num1 = Number(calculatorOuput.value);
 }
 
 function saveNum2OnOutput() {
-    num2 = Number(calculatorOuput.textContent);
+    num2 = Number(calculatorOuput.value);
 }
 
 // saves current operation on operands to num1
@@ -60,4 +58,21 @@ function resetAll() {
     num1 = undefined;
     num2 = undefined;
     operatorChosen = undefined;
+}
+
+// when operator is pressed
+// it will assign current number on output to num1 given ouput not empty
+// it will save operator chosen to operator var
+// if empty it does nothing
+
+// if num1 already occupied assign ouput on value to num2
+
+function saveNumberAndOperator(operatorPressed) {
+    if (isCalculatorOutputNotEmpty && num1 === undefined) {
+        saveNum1OnOutput();
+        saveOperatorPressed(operatorPressed);
+        clear();
+    } else if (num1 !== undefined ) {
+        saveNum2OnOutput();
+    };
 }
